ae8c73d2d8896c9a99886525f1c34f05bc83bcd6
Juan J Seb Garcia <juan.garcia@live.co.uk>
2017-10-17 16:25:11 +0100

    Added index redirect

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect
 import logic
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return redirect("https://github.com/juanjsebgarcia/WalletBalance", code=302)
 
 @app.route('/balance/<wallet>')
 def showWalletBalance(wallet):
